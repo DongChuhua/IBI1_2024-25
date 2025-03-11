e7004cb968febdfb4a7e631fcf9f13a1955c9c69
DongChuhua <chuhua.24@intl.zju.edu.cn>
2025-03-11 16:13:06 +0800

    working with variables

---
+++ Practical4/variables.py
@@ -0,0 +1,20 @@
+a=15
+#walk to the bus stop
+b=75
+#bus journey
+c=a+b
+d=90
+#drive
+e=5
+#walk from the car park
+f=d+e
+if c < f:
+    print("taking bus is quicker")
+else:
+    print("driving is quicker")
+#result: taking bus is quicker
+X = 3>2
+Y = 3<2
+W = X and Y
+print(W)
+#result: False
