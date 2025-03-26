ec80697d74e9fddcf8628a5b08ce157a596ce3f5
DongChuhua <chuhua.24@intl.zju.edu.cn>
2025-03-26 20:24:01 +0800

    design the window

--- Practical6/spatial_SIR.py
+++ Practical6/spatial_SIR.py
@@ -6,9 +6,7 @@ population = np.zeros((100,100))
 #randomly choose the location of the outbreak
 outbreak = np.random.choice(range(100),2)
 population[outbreak[0],outbreak[1]] = 1
-#create a kind of heat map
-plt.figure(figsize=(6,4),dpi=150)
-plt.imshow(population,cmap='viridis',vmin=0, vmax=1, interpolation='nearest')
+
 
 SP = 9999 #susceptible_population
 IP  = 1 #infected_population
@@ -19,12 +17,12 @@ gamma = 0.05 #recovery_rate
 
 #loop every point
 new_population=population.copy()
-for a in range(10):  #loop 100 times
+for a in range(100):  #loop 100 times
     #loop every point
     for x in range(100):
         for y in range(100):
-            #try to infect the neighbor of the infected person
             if population[x,y] == 1: #located the infected person
+                #try to infect the neighbor of the infected person
                 #locate its neighbor (ensure is within the size)
                 for i in range(max(0,x-1),min(100,x+2)):
                     for j in range(max(0,y-1),min(100,y+2)):
@@ -36,6 +34,8 @@ for a in range(10):  #loop 100 times
                 if np.random.choice(range(2),1,p=[1-gamma,gamma]).sum() == 1:
                     new_population[x,y] = 2
     population = new_population
+   #plot the heat map
+    plt.figure(figsize=(6,4),dpi=150)
     plt.imshow(population,cmap='viridis',vmin=0, vmax=2, interpolation='nearest')
     plt.show()
         
