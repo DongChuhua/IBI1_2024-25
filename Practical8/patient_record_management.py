c1185ddd855c7371099a8e2b8d7aa36952a62fb7
DongChuhua <chuhua.24@intl.zju.edu.cn>
2025-04-08 18:10:41 +0800

    manage patient record

---
+++ Practical8/patient_record_management.py
@@ -0,0 +1,15 @@
+#Create a class	called ‘patients’ 
+#which contains the following attributes: patient name; age; date of latest admission; and medical history
+class patients:
+    def __init__(self, name, age, admission_date, medical_history):
+        self.name = name
+        self.age = age
+        self.admission_date = admission_date
+        self.medical_history = medical_history
+
+    def details(self):
+        return f"Patient Name: {self.name}, Age: {self.age}, Admission Date: {self.admission_date}, Medical History: {self.medical_history}"
+
+# example usage:   
+patient_x = patients("DongChuhua", 18, "2025-04-08", "fever")
+print(patients.details(patient_x))
