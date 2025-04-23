5d2f155aca1bb9f3f68e72a6536fc0512cb39d86
DongChuhua <chuhua.24@intl.zju.edu.cn>
2025-04-23 15:14:16 +0800

    finish the task of Practical9

--- Practical9/try.py
+++
@@ -1,9 +0,0 @@
-import os
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-
-os.chdir('D:\IBI\IBI1_2024-25\Practical9')
-dalys_data = pd.read_csv('dalys-rate-from-all-causes.csv')
-print(dalys_data.iloc[0:2,:])
-print(dalys_data.head())
