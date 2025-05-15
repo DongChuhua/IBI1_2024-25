721ea907610160f0883bf71beb74636642a36cf1
DongChuhua <chuhua.24@intl.zju.edu.cn>
2025-05-15 20:48:12 +0800

    change the range

--- Practical4/mystery_code.py
+++ Practical4/mystery_code.py
@@ -1,8 +1,8 @@
 # What does this piece of code do?
-# Answer: print out the number of tries it took for two random numbers between 1 and 5 is equal.
+# Answer: print out the number of tries it took for two random numbers between 1 and 6 is equal.
 # Import libraries
 # randint allows drawing a random number,
-# e.g. randint(1,5) draws a number between 1 and 5
+# e.g. randint(1,6) draws a number between 1 and 6
 from random import randint
 
 # ceil takes the ceiling of a number, i.e. the next higher integer.
@@ -12,8 +12,8 @@ from math import ceil
 progress=0
 while progress>=0:
 	progress+=1
-	first_n = randint(1,5)
-	second_n = randint(1,5)
+	first_n = randint(1,6)
+	second_n = randint(1,6)
 	if first_n == second_n:
 		print(progress)
 		break
