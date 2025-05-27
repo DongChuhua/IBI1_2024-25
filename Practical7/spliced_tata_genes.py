9d1c2554ab8531d189fc4dd313c3ee6c1ad36663
DongChuhua <chuhua.24@intl.zju.edu.cn>
2025-05-27 21:30:22 +0800

    debug Practical7

--- Practical7/spliced_tata_genes.py
+++ Practical7/spliced_tata_genes.py
@@ -1,79 +1,41 @@
-#input three possible splice donor/acceptor	combinations (GTAG,	GCAG, ATAC)	
-#output different file with different name(GTAG_spliced_genes.fa, GCAG_spliced_genes.fa, ATAC_spliced_genes.fa)
-
-#for each combination
-#read the file line by line
-# if the line starts with ">", find the gene_name
-# if the line does not start with ">", append the sequence into one line
-#split the introns of each seq of the gene
-#count the number of introns
-#write the gene_name and sequence into the new file
-
 import re
 
-
-#define the function to cut introns from the sequence
-def cut_introns(seq, donor, acceptor):
-    introns = []
-    donor_sites = [m.start() for m in re.finditer(donor, seq)] #find all the start of donor_sites, store in list
-    for i in donor_sites:
-        A = re.search(acceptor, seq[i+2:]) #find all the acceptor 
-        if A:
-            new_introns = seq[i:A.start()+2+i+2]
-            introns.append(new_introns) 
-    return introns
-
-
-
-#input
-combination = str(input("Enter the splice donor/acceptor combination (GTAG, GCAG, ATAC): "))
-#create a dictionary to store the different combination
-donor_acceptor = {'GTAG': ('GT', 'AG'), 'GCAG': ('GC', 'AG'), 'ATAC': ('AT', 'AC')}
-donor, acceptor = donor_acceptor[combination]
-#ensure the validity of the input
-if combination not in donor_acceptor:
+# input splice donor/acceptor combination
+combination = input("Enter the splice donor/acceptor combination (GTAG, GCAG, ATAC): ").strip().upper()
+if combination not in ['GTAG', 'GCAG', 'ATAC']:
     print("Invalid combination!")
     exit()
 
+# Build regular expression pattern: first 2 bases+any character+last 2 bases
+splice_pattern = re.compile(combination[:2] + r'.*?' + combination[2:])
+# TATA box
+tata_pattern = re.compile(r'TATA[AT]A[AT]') 
 
-#initialize the variables
-seq = ''
-gene_name = ''
-total_introns = []
-
-
-if combination in donor_acceptor:
-    with open ('tata_genes.fa', "r") as file, open(f'{combination}_spliced_genes.fa', "w") as out:
-        #read line by line
-        for line in file:
-            line = line.strip()
-            if line.startswith('>'):
-                if seq: 
-                    total_introns = cut_introns(seq, donor, acceptor)  #cut the introns from the sequence
-                    # go through each intron in the total_introns list          
-                    for one_intron in total_introns:
-                        if gene_name and re.search(r'TATA[AT]A[AT]', one_intron):
-                            TATA_count = str(len(re.findall(r'TATA[AT]A[AT]', one_intron))) #count the number of TATA box sequence
-                            out.write('>' + gene_name[0] + ' number_of_TATA_box:' + TATA_count + '\n' + one_intron + '\n')
-                        
-                    gene_name = ''#reset the gene name for the next gene
-                    seq = '' #reset the sequence for the next gene
-                    total_introns = [] #reset
-                gene_name = re.findall(r'>(\S+)', line)
-            else: # for the sequence part
-                seq += line.strip()
-        
-        # check the last gene in the file
-        total_introns = cut_introns(seq, donor, acceptor)  #cut the introns from the sequence                     
-        for one_intron in total_introns:
-            if gene_name and re.search(r'TATA[AT]A[AT]', one_intron):
-                TATA_count = str(len(re.findall(r'TATA[AT]A[AT]', one_intron))) #count the number of TATA box sequence
-                out.write('>' + gene_name[0] + ' number_of_TATA_box:' + TATA_count + '\n' + one_intron + '\n')  
+with open('tata_genes.fa', 'r') as infile, open(f'{combination}_spliced_genes.fa', 'w') as outfile:
+    seq = ''
+    gene_name = 'Unknown'
     
-
-  
+    for line in infile:
+        line = line.strip()
         
-                    
-
-
-
+        if line.startswith('>'):
+            if seq:
+                # Check whether there are splice sites and at least 1 TATA box
+                if splice_pattern.search(seq):
+                    tata_matches = tata_pattern.findall(seq)
+                    if tata_matches:
+                        outfile.write(f'>{gene_name} number_of_TATA_box:{len(tata_matches)}\n{seq}\n')
+            
+            # start a new gene
+            seq = ''
+            gene_name = re.search(r'gene:(\S+)', line)
+        
+        else:
+            seq += line
+    
+    # check the last gene in the file
+    if seq and splice_pattern.search(seq):
+        tata_matches = tata_pattern.findall(seq)
+        if tata_matches:
+            outfile.write(f'>{gene_name} number_of_TATA_box:{len(tata_matches)}\n')
+            outfile.write(f'{seq}\n')
