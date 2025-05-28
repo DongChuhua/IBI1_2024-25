d894ef4a4c93d45ea42569031e3c66893aaa6d2a
DongChuhua <chuhua.24@intl.zju.edu.cn>
2025-05-28 09:12:43 +0800

    change a way to compute the largest intron

--- Practical7/largest_intron.py
+++ Practical7/largest_intron.py
@@ -1,28 +1,31 @@
+# Find the length of the largest possible intron in a DNA sequence. The intron must start with GT and end with AG.
 import re
-seq = 'ATGCAAGTGGTGTGTCTGTTCTGAGAGGGCCTAA'
 
-#locte every GT in the sequence
-#from the GT, try to find the next AG
-#the sequence between GT and AG is an intron (including GT and AG)
-#the length of the intron is len(AG:)-len(GT:)+1
-#find the next GT after the AG
-#loop until there is no AG or no GT
-#output the largest intron
-   
-
-# Find all start positions of 'GT' (splice-donor sites)
-GT_sites = [m.start() for m in re.finditer('GT', seq)]   
-largest_intron = ''
-for i in GT_sites: 
-    A =  re.search('AG', seq[i+2:])  # Search for 'AG' after 'GT'; A is the match object 
-    if A: # if match, A = ture
-        intron = seq [i:A.start()+2+i+2]
-        if len(intron) > len(largest_intron):
-            largest_intron = intron 
-len = len(largest_intron)
-print ('the largest intron is: ' + largest_intron)
-print ('The length of the largest intron is ' + str(len))
-  
+def find_largest_intron(seq):
+    max_length = 0
+    GT_positions = []
+    AG_positions = []
+    largest_intron = ""
+    
+    # Find all GT positions (splice donor sites)
+    GT_positions =  [m.start() for m in re.finditer('GT', seq)]
+    
+    # Find all AG positions (splice acceptor sites)
+    AG_positions = [m.start() for m in re.finditer('AG', seq)]
+    
+    # Check all possible GT-AG pairs where GT comes before AG
+    for GT in GT_positions:
+        for AG in AG_positions:
+            if AG > GT + 1:  # AG must be after GT with at least one base in between
+                intron_length = AG - GT + 2  # +2 to include both dinucleotides
+                if intron_length > max_length:
+                    max_length = intron_length
+                    largest_intron = seq[GT:AG + 2]
+    return max_length, largest_intron
 
 
-
+#  test cases
+test_sequences = 'ATGCAAGTGGTGTGTCTGTTCTGAGAGGGCCTAA'
+max_length, largest_intron = find_largest_intron(test_sequences)
+print('The largest intron is: ' + largest_intron)
+print ('The length of the largest intron is ' + str(max_length))
